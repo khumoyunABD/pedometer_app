1eca7f5f3ff50bb490ab8ff115ef6b4f425fa840
khumoyunABD <allhater37@gmail.com>
2024-06-05 20:54:27 +0500

    setting goal functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:provider/provider.dart';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,7 +9,9 @@ String formatDate(DateTime d) {
 }
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -27,12 +30,14 @@ class _MyAppState extends State<MyApp> {
   int _savedSteps = 0;
   bool _isCounting = true;
   int _totalStepsToday = 0;
+  int _dailyGoal = 10000; // Default goal
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
     _loadTotalSteps();
+    _loadDailyGoal();
   }
 
   Future<void> _loadTotalSteps() async {
@@ -52,6 +57,22 @@ class _MyAppState extends State<MyApp> {
   Future<void> _saveTotalSteps(int steps) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('total_steps', steps);
+  }
+
+  Future<void> _loadDailyGoal() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _dailyGoal =
+          prefs.getInt('daily_goal') ?? 10000; // Default to 10,000 steps
+    });
+  }
+
+  Future<void> _saveDailyGoal(int goal) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('daily_goal', goal);
+    setState(() {
+      _dailyGoal = goal;
+    });
   }
 
   void onStepCount(StepCount event) {
@@ -121,73 +142,128 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void _showGoalDialog() {
+    TextEditingController goalController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Set Daily Step Goal'),
+          content: TextField(
+            controller: goalController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(hintText: "Enter your goal"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Save'),
+              onPressed: () {
+                int goal = int.parse(goalController.text);
+                _saveDailyGoal(goal);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double progress = _totalStepsToday / _dailyGoal;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Pedometer Example'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Steps Taken',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                _steps == -1 ? 'Step count not available' : _steps.toString(),
-                style: const TextStyle(fontSize: 30, color: Colors.green),
-              ),
-              const Divider(
-                height: 100,
-                thickness: 0,
-                color: Colors.white,
-              ),
-              const Text(
-                'Total Steps Today',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                _totalStepsToday.toString(),
-                style: const TextStyle(fontSize: 30, color: Colors.blue),
-              ),
-              const Divider(
-                height: 100,
-                thickness: 0,
-                color: Colors.white,
-              ),
-              const Text(
-                'Pedestrian Status',
-                style: TextStyle(fontSize: 30),
-              ),
-              Icon(
-                _status == 'walking'
-                    ? Icons.directions_walk
-                    : _status == 'stopped'
-                        ? Icons.accessibility_new
-                        : Icons.error,
-                size: 100,
-              ),
-              Center(
-                child: Text(
-                  _status,
-                  style: _status == 'walking' || _status == 'stopped'
-                      ? const TextStyle(fontSize: 30)
-                      : const TextStyle(fontSize: 20, color: Colors.red),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Steps Taken',
+                  style: TextStyle(fontSize: 30),
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: toggleCounting,
-                child: Text(_isCounting ? 'Stop Counting' : 'Start Counting'),
-              ),
-              ElevatedButton(
-                onPressed: resetSteps,
-                child: const Text('Reset Steps'),
-              ),
-            ],
+                Text(
+                  _steps == -1 ? 'Step count not available' : _steps.toString(),
+                  style: const TextStyle(fontSize: 30, color: Colors.green),
+                ),
+                const Divider(
+                  height: 100,
+                  thickness: 0,
+                  color: Colors.white,
+                ),
+                const Text(
+                  'Total Steps Today',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Text(
+                  _totalStepsToday.toString(),
+                  style: const TextStyle(fontSize: 30, color: Colors.blue),
+                ),
+                const Divider(
+                  height: 100,
+                  thickness: 0,
+                  color: Colors.white,
+                ),
+                const Text(
+                  'Pedestrian Status',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Icon(
+                  _status == 'walking'
+                      ? Icons.directions_walk
+                      : _status == 'stopped'
+                          ? Icons.accessibility_new
+                          : Icons.error,
+                  size: 100,
+                ),
+                Center(
+                  child: Text(
+                    _status,
+                    style: _status == 'walking' || _status == 'stopped'
+                        ? const TextStyle(fontSize: 30)
+                        : const TextStyle(fontSize: 20, color: Colors.red),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: toggleCounting,
+                  child: Text(_isCounting ? 'Stop Counting' : 'Start Counting'),
+                ),
+                ElevatedButton(
+                  onPressed: resetSteps,
+                  child: const Text('Reset Steps'),
+                ),
+                ElevatedButton(
+                  onPressed: _showGoalDialog,
+                  child: const Text('Set Daily Goal'),
+                ),
+                const Text(
+                  'Daily Goal Progress',
+                  style: TextStyle(fontSize: 30),
+                ),
+                LinearProgressIndicator(
+                  value: progress,
+                  minHeight: 20,
+                ),
+                Text(
+                  '${(_totalStepsToday / _dailyGoal * 100).toStringAsFixed(1)}% of $_dailyGoal steps',
+                  style: const TextStyle(fontSize: 20, color: Colors.black),
+                ),
+                const SizedBox(
+                  height: 50,
+                )
+              ],
+            ),
           ),
         ),
       ),
